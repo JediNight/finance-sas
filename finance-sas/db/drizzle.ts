511da23b378638ed624263d07549ea5b8db820d5
Toks Fawibe <toksfawibe@Tokss-MacBook-Pro.local>
2024-06-04 12:31:09 -0500

    feat: added drizzle orm

---
+++ finance-sas/db/drizzle.ts
@@ -0,0 +1,11 @@
+import { neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
+
+import * as schema from "./schema";
+
+export const sql = neon(process.env.DATABASE_URL!);
+export const db = drizzle(sql);
+// const db = drizzle(sql, { schema });
+// const accounts2 = db.query.accounts.findFirst(where:{
+//     name: 'Checking'
+// });
